a481a38adff144ac29e3acf8e954522a25557ad1
Azeem Mohammad <azeemmohd@gmail.com>
2020-05-08 01:05:41 +0530

    j

--- img/speaker.js
+++ img/speaker.js
@@ -1,6 +1,6 @@
 var audiostatus = 'off';
 var video = $("#bannerVid").get(0);
-$(document).on('click touchend', '.speaker', function () {
+$(document).on('click', '.speaker', function () {
   /* Touchend is necessary for mobile devices, click alone won't work */
   if (!$('.speaker').hasClass("speakerplay")) {
     if (audiostatus == 'off') {
@@ -19,3 +19,23 @@ $(document).on('click touchend', '.speaker', function () {
     audiostatus = 'on';
   }
 });
+
+$(document).on('touchend', '.speaker', function () {
+    /* Touchend is necessary for mobile devices, click alone won't work */
+    if (!$('.speaker').hasClass("speakerplay")) {
+      if (audiostatus == 'off') {
+        $('.speaker').addClass('speakerplay');
+        video.muted = false;
+        window.clearTimeout(mouseovertimer);
+        audiostatus = 'on';
+        return false;
+      } else if (audiostatus == 'on') {
+        $('.speaker').addClass('speakerplay');
+        video.muted = false;
+      }
+    } else if ($('.speaker').hasClass("speakerplay")) {
+      $('.speaker').removeClass('speakerplay');
+      video.muted = true;
+      audiostatus = 'on';
+    }
+  });
