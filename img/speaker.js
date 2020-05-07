e2c50465189fc15842b4f9fb436d145e3de6fb64
Azeem Mohammad <azeemmohd@gmail.com>
2020-05-08 01:40:05 +0530

    rr

--- img/speaker.js
+++ img/speaker.js
@@ -1,41 +1,42 @@
 var audiostatus = 'off';
 var video = $("#bannerVid").get(0);
-$(document).on('click', '.speaker', function () {
-  /* Touchend is necessary for mobile devices, click alone won't work */
-  if (!$('.speaker').hasClass("speakerplay")) {
-    if (audiostatus == 'off') {
-      $('.speaker').addClass('speakerplay');
-      video.muted = false;
-      window.clearTimeout(mouseovertimer);
-      audiostatus = 'on';
-      return false;
-    } else if (audiostatus == 'on') {
-      $('.speaker').addClass('speakerplay');
-      video.muted = false;
-    }
-  } else if ($('.speaker').hasClass("speakerplay")) {
-    $('.speaker').removeClass('speakerplay');
-    video.muted = true;
-    audiostatus = 'on';
-  }
-});
 
-$(document).on('touchend', '.speaker', function () {
+$(document).on('click', '.speaker', function (e) {
     /* Touchend is necessary for mobile devices, click alone won't work */
     if (!$('.speaker').hasClass("speakerplay")) {
-      if (audiostatus == 'off') {
-        $('.speaker').addClass('speakerplay');
-        video.muted = false;
-        window.clearTimeout(mouseovertimer);
-        audiostatus = 'on';
-        return false;
-      } else if (audiostatus == 'on') {
-        $('.speaker').addClass('speakerplay');
-        video.muted = false;
-      }
+        if (audiostatus == 'off') {
+            $('.speaker').addClass('speakerplay');
+            video.muted = false;
+            window.clearTimeout(mouseovertimer);
+            audiostatus = 'on';
+            return false;
+        } else if (audiostatus == 'on') {
+            $('.speaker').addClass('speakerplay');
+            video.muted = false;
+        }
     } else if ($('.speaker').hasClass("speakerplay")) {
-      $('.speaker').removeClass('speakerplay');
-      video.muted = true;
-      audiostatus = 'on';
+        $('.speaker').removeClass('speakerplay');
+        video.muted = true;
+        audiostatus = 'on';
     }
-  });
+});
+
+$(".speaker").on("tap", function () {
+    /* Touchend is necessary for mobile devices, click alone won't work */
+    if (!$('.speaker').hasClass("speakerplay")) {
+        if (audiostatus == 'off') {
+            $('.speaker').addClass('speakerplay');
+            video.muted = false;
+            window.clearTimeout(mouseovertimer);
+            audiostatus = 'on';
+            return false;
+        } else if (audiostatus == 'on') {
+            $('.speaker').addClass('speakerplay');
+            video.muted = false;
+        }
+    } else if ($('.speaker').hasClass("speakerplay")) {
+        $('.speaker').removeClass('speakerplay');
+        video.muted = true;
+        audiostatus = 'on';
+    }
+});
